7798cf4b0b367fbf3d65a531477ec44732b147a8
Sebastián Vallejo <sebasvallejo@gmail.com>
2025-10-15 13:20:40 -0500

    Update cors

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -1,10 +1,14 @@
 import cors from "cors";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 dotenv.config();
 
+const allowedOrigins = process.env.CORS_ORIGIN
+  ? process.env.CORS_ORIGIN.split(",").map(origin => origin.trim())
+  : [];
+
 export const corsMiddleware = cors({
-  origin: process.env.CORS_ORIGIN,
+  origin: allowedOrigins,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
 });
