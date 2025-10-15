ae4531a10d5cea4918204a05dcf4efed755300e2
Sebastián Vallejo <sebasvallejo@gmail.com>
2025-10-15 03:50:39 -0500

    SSL update 2

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -4,24 +4,24 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    ssl: {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  ssl: {
     rejectUnauthorized: false
   }
 });
 
 export const connectDatabase = async (): Promise<void> => {
-    try {
-      const client = await pool.connect(); // obtenemos una conexión del pool
-      await client.query("SELECT 1");      // consulta de prueba
-      client.release();                    // liberamos la conexión
-      console.log("✅ Database connection established");
-    } catch (error) {
-      console.error("❌ Unable to connect to the database:", error);
-      throw error;
-    }
-  };
+  try {
+    const client = await pool.connect(); // obtenemos una conexión del pool
+    await client.query("SELECT 1"); // consulta de prueba
+    client.release(); // liberamos la conexión
+    console.log("✅ Database connection established");
+  } catch (error) {
+    console.error("❌ Unable to connect to the database:", error);
+    throw error;
+  }
+};
